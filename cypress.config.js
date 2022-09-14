51f732aa9e9c2d75fa85bf904a9215b43b86d088
enzofurlan <enzoofurlan@gmail.com>
2022-09-14 02:07:26 -0300

    - Remove video recording.

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,8 @@ const { defineConfig } = require('cypress');
 module.exports = defineConfig({
     e2e: {
         baseUrl: 'http://localhost:3000/',
+        video: false,
+        videoUploadOnPasses: false,
         setupNodeEvents(on, config) {
             // implement node event listeners here
         },
